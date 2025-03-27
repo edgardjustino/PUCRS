ac535a56981f48f1960939663160afdc725fd6c7
edgardjustino <edgardpjustino@gmail.com>
2025-03-26 21:21:59 -0300

    testing contributions on github

--- Fundamentos-da-computacao-e-algoritmos/edson-moreno/recursao.js
+++ Fundamentos-da-computacao-e-algoritmos/edson-moreno/recursao.js
@@ -1,9 +1,6 @@
 function doSomething(value) {
-if (value == 0) 
-  return 0;
-else
-return value + doSomething(value - 1);
-
+  if (value == 0) return 0;
+  else return value + doSomething(value - 1);
 }
 
-doSomething(3)
+doSomething(3);
--- Fundamentos-da-computacao-e-algoritmos/edson-moreno/teste.js
+++ Fundamentos-da-computacao-e-algoritmos/edson-moreno/teste.js
@@ -1,8 +1,8 @@
 var x = 0;
-var n = 0; 
-while (n<3) {
+var n = 0;
+while (n < 3) {
   n = n + 1;
   x = n + x;
 }
 
-console.log(x)
+console.log(x);
